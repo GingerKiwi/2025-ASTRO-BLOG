4212ba8147905763467e5cfcbc330e8479160fbc
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2025-06-21 18:12:30 -0400

    run import image from '@astrojs/image'. and update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import image from '@astrojs/image';
+
+export default defineConfig({
+  integrations: [image()],
+});
+
 // https://astro.build/config
 export default defineConfig({});

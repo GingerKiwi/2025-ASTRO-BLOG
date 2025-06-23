13c0a3fd1dccd3f441caeb13a9bde734db7b89a4
Liz GingerKiwi.Dev <liz@gingerkiwi.dev>
2025-06-23 13:06:01 -0400

    fix all errors except not finding BlogLayout.astro. Time to do something else

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@
 import { defineConfig } from 'astro/config';
 import path from 'node:path'; // ✅ Required for path.resolve()
 import sitemap from '@astrojs/sitemap';
-import content from '@astrojs/content';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap(), content()],
+  integrations: [sitemap(),],
   alias: {
     '~/layouts': path.resolve('./src/layouts'), // ✅ This needs `path` defined
   },
